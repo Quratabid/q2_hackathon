f84ec9ac8927da097732389a0845eca3ab51ef5b
Quratabid <162566815+Quratabid@users.noreply.github.com>
2025-02-08 22:19:55 +0500

    Hero.tsx

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -4,24 +4,25 @@ import { FaStar } from 'react-icons/fa'
 import { FaStarHalfAlt } from "react-icons/fa";
 const Hero = () => {
   return (
-    <div className='w-screen'>
+    <div className=''>
       
-      <div className=' flex bg-[#F2F0F1] gap-52 p-5 mt-5 flex-col lg:flex-row  '>
+      <div className=' flex bg-[#F2F0F1] sm:items-center lg:gap-36 lg:px-10 xl:px-44  p-5 mt-5 flex-col lg:flex-row  '>
       <div>
-        <h1 className=' font-bold  md:font-extrabold text-[36px]   md:text-[52px] mx-5'>Find Clothes<br />That Matches<br />Your Style</h1>
+        <h1 className=' font-bold  md:font-extrabold  text-[28px]sm:text-[36px]  md:text-[40px] lg:text-[52px] px-5'>Find Clothes<br />That Matches<br />Your Style</h1>
         
-        <p className='mx-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.  Aperiam modi fugi<br /> perferendis adipisci  illo atque culpa   .</p>
+        <p className=''>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />
+         Aperiam modi fugi perferendis adipisci  illo atque culpa   .</p>
        
         <button className=' rounded-full bg-black text-white
         px-20  py-2 mt-5 '>Shop Now</button></div>
        
          <div>
-          <Image src="/h2.png" alt='' className='pt-5' height={448} width={398}/>
+          <Image src="/h2.png" alt='' className=' ' height={448} width={398}/>
          </div>
 
       </div>
       
-      <div className=' bg-black w-screen flex gap-20 h-20 justify-center p-4 flex-wrap md:flex-nowrap '>
+      <div className=' bg-black w-full flex gap- h-20 justify-center p-4 flex-wrap md:flex-nowrap '>
 <Image src="/v.png" alt='' height={33} width={160}/>
 <Image src="/v2.png" alt='' height={38} width={90}/>
 <Image src="/v3.png" alt='' height={36} width={155}/>
@@ -32,12 +33,12 @@ const Hero = () => {
 
       {/*  */}
       <div className='flex  justify-center'>
-        <h1 className='text-[48px] font-bold pt-6' >New Arrivals</h1>
+        <h1 className='text-[48px] font-bold pt-5' >New Arrivals</h1>
       </div>
 
-      <div className='flex flex-col md:flex-row p-10 gap-20 pt-3 justify-center font-poppins text-[#000000] font-semibold'>
+      <div className='flex flex-col md:flex-row p-10 gap-10 pt-3 justify-center font-poppins text-[#000000] font-semibold'>
         <div className='w-[270px] h-[350px]'>
-        <Image src="/c.png" alt='' height={250} width={270}/>
+        <Image src="/c3.png" alt='' height={250} width={270}/>
         <h1 className='mt-3'>Kids T-Shirt</h1>
         <h1 className='text-[#DB4444] text-[16px]'>$289</h1>
         <div className='mt-2 flex space-x-1 '>
@@ -86,15 +87,16 @@ const Hero = () => {
         </div>
         
         </div>
+        
 
 
         {/* 2nd row  top selling*/}
         <div className='flex justify-center'>
-        <h1 className='text-[48px] font-bold pt-10' >Top Selling
+        <h1 className='text-[48px] font-bold ' >Top Selling
 
         </h1>
       </div>
-        <div className='flex flex-col p-10 md:flex-row gap-20 justify-center pt-5'>
+        <div className='flex flex-col p-10 md:flex-row gap-10 justify-center pt-5'>
         <div className='w-[270px] h-[350px]'>
           <Image src="/c3.png" alt='' height={250} width={270}/>
           <h1 className='mt-3'>Brand</h1>
@@ -125,7 +127,7 @@ const Hero = () => {
           </div>
         </div>
         <div className='w-[270px] h-[350px]'>
-          <Image src="/c.png" alt='' height={250} width={270}/>
+          <Image src="/c1.png" alt='' height={250} width={270}/>
           <h1 className='mt-3'>T-shirt</h1>
           <h1 className='text-[#DB4444]'>$269</h1>
           
@@ -159,7 +161,7 @@ const Hero = () => {
       </div>
 
       <div className='flex text-center  bg-[#F0F0F0] 
-       justify-center font-bold   text-[32px]  md:text-[48px] mt-14 pt-7 '>
+       justify-center font-bold   text-[32px]  md:text-[48px] mt-5 pt-7 '>
         <h1 className=''>BROWSE BY DRESS STYLE</h1>
         </div>
         <div>
@@ -174,8 +176,61 @@ const Hero = () => {
         <Image src="/a4.png" alt='' height={289} width={407}/>
         </div>
         </div>
+        <div>
 
-     
+        
+        <h1 className=' text-[32px] md:text-[48px]  p-10 font-bold '>OUR HAPPY CUSTOMERS</h1></div>
+        <div className='  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center  '>
+        <div className=''>
+         
+        <div className='mt- mx-20 flex space-x-3 '>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <  FaStarHalfAlt className='text-yellow-500'/>
+          </div>
+        <h1 className='text-[20px]  font-bold mx-20 mt-3'>Sara M.</h1>
+        
+        <p className=' mx-20 text-[16px] text-gray-400 '>I am blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses every piece I have bought has exceeded my expectations.</p>
+        </div>
+        <div className=''>
+        <div className='mt-2 mx-20 flex space-x-3 '>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <  FaStarHalfAlt className='text-yellow-500'/>
+          </div>
+        <h1 className='text-[20px]  font-bold mx-20 mt-3'>Alex K.</h1>
+        
+        <p className=' px-20 text-[16px] text-gray-400 '>Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable  catering to a variety of tastes and occasions.</p>
+        </div>
+        <div className=''>
+        <div className='mt-2 mx-20 flex space-x-3 '>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <FaStar className='text-yellow-500'/>
+            <  FaStarHalfAlt className='text-yellow-500'/>
+          </div>
+        <h1 className='text-[20px]  font-bold mx-20 mt-3'>James L.</h1>
+        
+        <p className=' mx-20 text-[16px] text-gray-400 '>As someone who is always on the lookout for unique fashion pieces. I am thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.</p>
+        </div>
+      </div>
+<div       className='bg-black md:h-[180px]  w-full  flex flex-col md:flex-row  justify-center lg:mt-9 p-5  lg:pl-5'>
+        <div className='flex-1'>
+        <h1 className='font-bold text-[32px]  lg:text-[40px] text-white '>STAY UPTO DATE ABOUT  <br />OUR LATEST OFFERS</h1></div>
+       
+   <div className='flex-1 space-y-7 '>
+<input type="" className='rounded-full text-center border 
+w-[280px] h-[42px] lg:w-[349px] lg:h-[46px] border-gray-400 mt-5 ' 
+placeholder='Enter your email' />
+<div className='border border-gray-400 rounded-full 
+lg:w-[349px] lg:h-[46px] w-[280px] h-[42px] p-2 text-black lg:pt-3 bg-white text-center items-center '>
+  Subscribe to Newsletter</div></div>
+      </div>
     </div>
   )
 }
